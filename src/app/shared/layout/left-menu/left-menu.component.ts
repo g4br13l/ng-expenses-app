174fa4ac467ffcd067ed39384a0da4dfb34cccce
Gabriel <gabrielnascimento.lima@portoseguro.com.br>
2023-12-23 18:14:55 -0300

    favicon, ajust bottom menu an lef menu

--- src/app/shared/layout/left-menu/left-menu.component.ts
+++ src/app/shared/layout/left-menu/left-menu.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {NgOptimizedImage} from "@angular/common";
 
 @Component({
   selector: 'app-left-menu',
   standalone: true,
-  imports: [],
+  imports: [
+    NgOptimizedImage
+  ],
   templateUrl: './left-menu.component.html'
 })
 export class LeftMenuComponent {
--- src/app/shared/layout/top-menu/top-menu.component.ts
+++ src/app/shared/layout/top-menu/top-menu.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import {NgOptimizedImage} from "@angular/common";
 
 @Component({
   selector: 'app-top-menu',
   standalone: true,
   imports: [
+    NgOptimizedImage
   ],
   templateUrl: './top-menu.component.html'
 })
